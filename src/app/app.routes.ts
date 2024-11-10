5293e512afbcf27bc6d67fe3d6e42083be60665f
Roman Skydan <roma.skidan@gmail.com>
2024-11-10 17:50:40 +0200

    Create component stubs for ContactList and ContactForm and configure Angular routes for their display

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+  { path: 'contacts', component: ContactsListComponent },
+  { path: 'contacts/:id', component: ContactFormComponent },
+];
